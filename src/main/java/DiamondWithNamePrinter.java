c5283b0839519275a178d2590543f1412df67a05
idolice <349031587@qq.com>
2017-02-15 14:33:51 +0800

    [Bing]finished all the exercises

---
+++ src/main/java/DiamondWithNamePrinter.java
@@ -0,0 +1,40 @@
+package src.main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bingwang on 2/15/17.
+ */
+public class DiamondWithNamePrinter {
+    public void print(int number) {
+        int longest = (number - 1) * 2 + 1;
+        List numArray = new ArrayList();
+        List diamondArray = new ArrayList();
+        for (int i = 0; i < number; i++) {
+            numArray.add(i * 2 + 1);
+        }
+        for (int j = 0; j < number-1; j++) {
+            int space = longest - (int) numArray.get(j);
+            String temp = "";
+            for (int m = 0; m < space / 2; m++) {
+                temp += " ";
+            }
+            for (int n = 0; n < (int) numArray.get(j); n++) {
+                temp += "*";
+            }
+            for (int m = 0; m < space / 2; m++) {
+                temp += " ";
+            }
+            diamondArray.add(temp);
+            System.out.print(temp);
+            System.out.print("\n");
+
+        }
+            System.out.print("idolice");
+        for (int p = diamondArray.size() - 1; p >= 0; p--) {
+            System.out.print("\n");
+            System.out.print(diamondArray.get(p));
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,11 @@ public class Main {
     public static void main(String[] args){
         IsoscelesTrianglePrinter isoscelesTrianglePrinter = new IsoscelesTrianglePrinter();
         DiamondPrinter diamondPrinter = new DiamondPrinter();
+        DiamondWithNamePrinter diamondWithNamePrinter = new DiamondWithNamePrinter();
         Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
         isoscelesTrianglePrinter.print(input);
         diamondPrinter.print(input);
+        diamondWithNamePrinter.print(input);
     }
 }
